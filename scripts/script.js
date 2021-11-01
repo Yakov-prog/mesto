8b7b1b203fc843332cea7c177b6ecdb8e4470a14
Yakov Burachenko <yasha.burachenko@yandex.ru>
2021-11-01 04:19:40 +0300

    fix:Исправить переполнение профиля и изменить функции вызова popup а

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,42 @@
 let profileEditBtn = document.querySelector('.profile-info__edit-button');
 let profilePopup = document.querySelector('.popup');
-let openProfilePopup = document.querySelector('.popup_opened');
 let closeProfilePopup = document.querySelector('.popup-content__close-btn');
 let profileForm = document.querySelector('.popup-content__form');
 let profileName = document.querySelector('.profile-info__name');
 let profileProfession = document.querySelector('.profile-info__profession');
-let profileNameInput = document.querySelector('.popup-content__input_name');
-let profileProfessionInput = document.querySelector('.popup-content__input_profession');
+let profileNameInput = document.querySelector('.popup-content__input_value_name');
+let profileProfessionInput = document.querySelector('.popup-content__input_value_profession');
 let itemsLike = document.querySelectorAll('.grid-list__item-like');
-let itemLiked = document.querySelector('.grid-list__item-like_liked');
+// let itemLiked = document.querySelector('.grid-list__item-like_liked');
 
 
-function togglePopup() {
-    profilePopup.classList.toggle('popup_opened');
+function openPopup() {
+    profileNameInput.value = profileName.textContent;
+    profileProfessionInput.value = profileProfession.textContent;
+    profilePopup.classList.add('popup_opened');
 }
 
-profileEditBtn.addEventListener("click", togglePopup);
-closeProfilePopup.addEventListener("click", togglePopup);
+function closePopup() {
+    profilePopup.classList.remove('popup_opened');
+}
 
-function profileEdit (event) {
+profileEditBtn.addEventListener("click", openPopup);
+closeProfilePopup.addEventListener("click", closePopup);
+
+function profileEdit(event) {
     event.preventDefault();
         profileName.textContent = profileNameInput.value;
         profileProfession.textContent = profileProfessionInput.value;
-        togglePopup ();    
+        closePopup ();    
 }
 
 profileForm.addEventListener("submit", profileEdit);
 
-function togglelike() {
+/* function togglelike() {
 itemsLike.forEach((itemLike) => {
     itemLike.addEventListener("click", () => itemLike.classList.toggle('grid-list__item-like_liked'));
 })
 }
 
-togglelike();
+togglelike(); */
+

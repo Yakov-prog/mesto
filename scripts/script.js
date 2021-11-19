bbf45d7fc37b1d0a47aed7ee671ae6cb331cd7c3
Yakov Burachenko <yasha.burachenko@yandex.ru>
2021-11-19 15:15:52 +0300

    feat: добавить карточки и лайки к ним через js

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,60 @@
-let profileEditBtn = document.querySelector('.profile-info__edit-button');
-let profilePopup = document.querySelector('.popup');
-let closeProfilePopup = document.querySelector('.popup-content__close-btn');
-let profileForm = document.querySelector('.popup-content__form');
-let profileName = document.querySelector('.profile-info__name');
-let profileProfession = document.querySelector('.profile-info__profession');
-let profileNameInput = document.querySelector('.popup-content__input_value_name');
-let profileProfessionInput = document.querySelector('.popup-content__input_value_profession');
-let itemsLike = document.querySelectorAll('.grid-list__item-like');
-// let itemLiked = document.querySelector('.grid-list__item-like_liked');
+const profileEditBtn = document.querySelector('.profile-info__edit-button');
+const profilePopup = document.querySelector('.popup');
+const closeProfilePopup = document.querySelector('.popup-content__close-btn');
+const profileForm = document.querySelector('.popup-content__form');
+const profileName = document.querySelector('.profile-info__name');
+const profileProfession = document.querySelector('.profile-info__profession');
+const profileNameInput = document.querySelector('.popup-content__input_value_name');
+const profileProfessionInput = document.querySelector('.popup-content__input_value_profession');
+const itemsLike = document.querySelectorAll('.grid-list__item-like');
+const photoList = document.querySelector('.grid-list');
+const photoTemplate = document.querySelector('.grid-list__template');
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+
+function photoContent() {
+  initialCards.forEach((initialCard) => {
+    renderPhoto(initialCard);
+})
+}
+  
+function changeLike(event) {
+  event.target.classList.toggle('grid-list__item-like_liked');
+} 
+
+function renderPhoto(card) {
+    const cloneTemplate = photoTemplate.content.cloneNode(true);
+    cloneTemplate.querySelector('.grid-list__item-name').textContent = card.name;
+    cloneTemplate.querySelector('.grid-list__image').setAttribute('src', card.link);
+    cloneTemplate.querySelector('.grid-list__image').setAttribute('alt', card.name);
+    cloneTemplate.querySelector('.grid-list__item-like').addEventListener("click", changeLike);
+    photoList.appendChild(cloneTemplate);
+}
 
 
 function openPopup() {
@@ -32,11 +79,4 @@ function profileEdit(event) {
 
 profileForm.addEventListener("submit", profileEdit);
 
-/* function togglelike() {
-itemsLike.forEach((itemLike) => {
-    itemLike.addEventListener("click", () => itemLike.classList.toggle('grid-list__item-like_liked'));
-})
-}
-
-togglelike(); */
-
+photoContent();

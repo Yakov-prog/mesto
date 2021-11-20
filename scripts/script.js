8ea148c5b16c04ea80778b6541ced6d4fd69573a
Yakov Burachenko <yasha.burachenko@yandex.ru>
2021-11-20 14:57:53 +0300

    feat: добавить попап карточек и функцию редактирования

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,17 @@
 const profileEditBtn = document.querySelector('.profile-info__edit-button');
 const profilePopup = document.querySelector('.popup');
-const closeProfilePopup = document.querySelector('.popup-content__close-btn');
 const profileForm = document.querySelector('.popup-content__form');
+const closeProfilePopup = document.querySelector('.popup-content__close-btn');
 const profileName = document.querySelector('.profile-info__name');
 const profileProfession = document.querySelector('.profile-info__profession');
 const profileNameInput = document.querySelector('.popup-content__input_value_name');
 const profileProfessionInput = document.querySelector('.popup-content__input_value_profession');
-const itemsLike = document.querySelectorAll('.grid-list__item-like');
+const cardPopup = document.querySelector('.popup-card');
+const cardEditBtn = document.querySelector('.profile__add-button');
+const cardCloseBtn = document.querySelector('.popup-content__close-btn_card');
+const cardFormContent = document.querySelector('.popup-content__form_card');
+const cardNameInput = document.querySelector('.popup-content__input_value_card');
+const cardLinkInput = document.querySelector('.popup-content__input_value_link');
 const photoList = document.querySelector('.grid-list');
 const photoTemplate = document.querySelector('.grid-list__template');
 const initialCards = [
@@ -36,14 +41,13 @@ const initialCards = [
     }
   ];
 
-
 function photoContent() {
   initialCards.forEach((initialCard) => {
     renderPhoto(initialCard);
 })
 }
   
-function changeLike(event) {
+function toggleLike(event) {
   event.target.classList.toggle('grid-list__item-like_liked');
 } 
 
@@ -52,10 +56,21 @@ function renderPhoto(card) {
     cloneTemplate.querySelector('.grid-list__item-name').textContent = card.name;
     cloneTemplate.querySelector('.grid-list__image').setAttribute('src', card.link);
     cloneTemplate.querySelector('.grid-list__image').setAttribute('alt', card.name);
-    cloneTemplate.querySelector('.grid-list__item-like').addEventListener("click", changeLike);
-    photoList.appendChild(cloneTemplate);
+    cloneTemplate.querySelector('.grid-list__item-like').addEventListener("click", toggleLike);
+    photoList.prepend(cloneTemplate);
 }
 
+cardFormContent.addEventListener("submit", cardEdit);
+
+function cardEdit (event) {
+  event.preventDefault();
+  const newCards = {
+      name: cardNameInput.value,
+      link: cardLinkInput.value
+    };
+    renderPhoto(newCards);
+  closePopup ();   
+}
 
 function openPopup() {
     profileNameInput.value = profileName.textContent;
@@ -65,6 +80,7 @@ function openPopup() {
 
 function closePopup() {
     profilePopup.classList.remove('popup_opened');
+    closeCardPopup();
 }
 
 profileEditBtn.addEventListener("click", openPopup);
@@ -76,6 +92,17 @@ function profileEdit(event) {
         profileProfession.textContent = profileProfessionInput.value;
         closePopup ();    
 }
+
+function openCardPopup() {
+  cardPopup.classList.add('popup_opened');
+}
+
+function closeCardPopup() {
+  cardPopup.classList.remove('popup_opened');
+}
+
+cardEditBtn.addEventListener("click", openCardPopup);
+cardCloseBtn.addEventListener("click", closeCardPopup);
 
 profileForm.addEventListener("submit", profileEdit);
 
